d9dc3c38ae19ec06162bd57f0afc8bf37b97531b
alitze2 <alitze2@medtronic.com>
2023-07-10 15:59:46 +0300

    styles updated

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
 
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'con'
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header/>
-        <MarketingApp/>
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassname}>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <MarketingApp/>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
