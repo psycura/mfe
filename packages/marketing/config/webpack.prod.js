8c8feceb6db723fe62075b231df5730703723917
alitze2 <alitze2@medtronic.com>
2023-07-10 14:54:35 +0300

    added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -9,7 +9,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
