c7025f4e745a83e0f9aa424641e627ba6d93295f
alitze2 <alitze2@medtronic.com>
2023-07-11 14:13:24 +0300

    dashboard completed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,9 @@
-import React, { lazy, Suspense, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import Header from "./components/Header";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Router, Redirect, Switch, Route } from "react-router-dom";
 import { createGenerateClassName, StylesProvider } from "@material-ui/core";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from 'history';
 
 const generateClassname = createGenerateClassName({
   productionPrefix: 'con'
@@ -10,14 +11,23 @@ const generateClassname = createGenerateClassName({
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
+const history = createBrowserHistory();
 export default () => {
 
   const [isSignedIn, setSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn]);
+
+
   return (
     <StylesProvider generateClassName={generateClassname}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header signedIn={isSignedIn} onSignOut={() => setSignedIn(false)}/>
           <Suspense fallback={<Progress/>}>
@@ -25,11 +35,15 @@ export default () => {
               <Route path='/auth'>
                 <AuthLazy onSignIn={() => setSignedIn(true)}/>
               </Route>
+              <Route path='/dashboard'>
+                {!isSignedIn && <Redirect to='/'/>}
+                <DashboardLazy/>
+              </Route>
               <Route path='/' component={MarketingLazy}/>
             </Switch>
           </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
